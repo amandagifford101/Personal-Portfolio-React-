6ec93bbb11d3a5ae25ba83ccb7823208bbb7f53e
Amanda <agifford887@gmail.com>
2023-12-06 16:09:33 -0800

    graphic add all

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,13 +9,14 @@ const Hero = () => {
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex
       flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'></div>
-        <div className='w-5 h-5 rounded-full bg-[#e7dfdd]' />
+        <div className='w-5 h-5 rounded-full bg-[#0ee1f1]' />
         <div className="w-1 sm:h-80 h-40 violet-gradient" />
         <div>
           <h1 className={`${styles.heroHeadText} text-[#c63999]`}>Hi, I'm <span className='text-[#0ee1f1]'>Amanda</span></h1>
-          <p className={`${styles.heroSubText} mt-2 text-[#e7dfdd]`}>I develop the best applications.</p>
+          <p className={`${styles.heroSubText} mt-6 text-[#e7dfdd]`}>Exploring curiosity, <br className='sm:block hidden' />unleashing innovation.</p>
         </div>
       </div>
+      <ComputersCanvas />
     </section>
   )
 }
